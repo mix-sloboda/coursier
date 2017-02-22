7aed9e1fd87b8b1a831c7853cbf8c86e46cd1514
Alexandre Archambault <alexandre.archambault@gmail.com>
2017-02-22 14:54:25 +0100

    debug

--- sbt-launcher/src/main/scala/coursier/sbtlauncher/DummyGlobalLock.scala
+++ sbt-launcher/src/main/scala/coursier/sbtlauncher/DummyGlobalLock.scala
@@ -4,6 +4,8 @@ import java.io.File
 import java.util.concurrent.Callable
 
 case object DummyGlobalLock extends xsbti.GlobalLock {
-  def apply[T](lockFile: File, run: Callable[T]): T =
+  def apply[T](lockFile: File, run: Callable[T]): T = {
+    Console.err.println(s"** Asked a lock on $lockFile **")
     run.call()
+  }
 }
