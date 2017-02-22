26646a6477c05a455a2def71b7d63ccf000d2e95
Alexandre Archambault <alexandre.archambault@gmail.com>
2017-02-22 15:03:10 +0100

    meh global locking

--- sbt-launcher/src/main/scala/coursier/sbtlauncher/DummyGlobalLock.scala
+++ sbt-launcher/src/main/scala/coursier/sbtlauncher/DummyGlobalLock.scala
@@ -1,11 +1,23 @@
 package coursier.sbtlauncher
 
 import java.io.File
-import java.util.concurrent.Callable
+import java.util.concurrent.{Callable, ConcurrentHashMap}
 
 case object DummyGlobalLock extends xsbti.GlobalLock {
-  def apply[T](lockFile: File, run: Callable[T]): T = {
-    Console.err.println(s"** Asked a lock on $lockFile **")
-    run.call()
-  }
+
+  private val locks = new ConcurrentHashMap[File, AnyRef]
+
+  def apply[T](lockFile: File, run: Callable[T]): T =
+    Option(lockFile) match {
+      case None => run.call()
+      case Some(lockFile0) =>
+        Console.err.println(s"** Asked a lock on $lockFile **")
+
+        val lock0 = new AnyRef
+        val lock = Option(locks.putIfAbsent(lockFile0, lock0)).getOrElse(lock0)
+
+        lock.synchronized {
+          run.call()
+        }
+    }
 }
